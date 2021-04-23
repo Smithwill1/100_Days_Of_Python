3d4773b7380732235aaaa108e02cd5a916d0c306
William Smith <williamssmith526@gmail.com>
2021-04-23 10:10:01 +0100

    Day 17 start - object quiz

---
+++ Day_17/main.py
@@ -0,0 +1,17 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for item in question_data:
+    question = Question(item["text"], item["answer"])
+    question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+quiz.ask_question()
+
+while quiz.still_has_questions():
+    quiz.ask_question()
+
+print(question_bank)
